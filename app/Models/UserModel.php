5dd642e58ec6a4bfa385ae1af7dc047b1a8e85fa
cindy vira safitri <cindyvira929@gmail.com>
2024-05-03 08:45:45 +0700

    praktikum1 JS 10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,19 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends \Illuminate\Foundation\Auth\User
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+
     use HasFactory;
 
-    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
-
-    /**
-     * The attributes that are mass assignable.
-     * 
-     * @var array
-     */
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
     public function level(): BelongsTo
@@ -25,3 +32,23 @@ class UserModel extends \Illuminate\Foundation\Auth\User
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 }
+
+// class UserModel extends \Illuminate\Foundation\Auth\User
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+
+//     /**
+//      * The attributes that are mass assignable.
+//      * 
+//      * @var array
+//      */
+//     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+//     public function level(): BelongsTo
+//     {
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+// }
